bfe18467345868585bf9c4cf5828781a3d1efa34
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-12 16:44:04 -0700

    added dependent: :destroy for foreign key refs

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ActiveRecord::Base
-	has_many :person_with_roles
+	has_many :person_with_roles, dependent: :destroy
 	has_many :people, through: :person_with_roles
 
 	validates :role, presence: true, uniqueness: true
