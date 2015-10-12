1dcd39db60889618fbda731d9d4afe8ff5b136dd
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-12 16:15:17 -0700

    added validations

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,5 @@
 class Role < ActiveRecord::Base
+	validates :role, presence: true, uniqueness: true
+
+	# Should define a method that transforms input to be downcase only to ensure there are no duplicate roles i.e. "Actor" and "actor"
 end
